345dca0d8d95085195c127cd0cd0c41148dbdd20
xshiii <xshi@kth.se>
2018-02-08 11:34:06 +0100

    .

--- cExtension/setup.py
+++ cExtension/setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup, Extension
-
+import numpy as np
 module1 = Extension('demo',
-                    sources = ['demomodule.c']
+                    sources = ['demomodule.c'],
+                    include_dirs=[numpy.get_include()]
                     )
 
 setup (name = 'a demo extension module',
        version = '1.0',
        description = 'This is a demo package',
-       ext_modules = [module1])
+       ext_modules = [module1]
+       )
